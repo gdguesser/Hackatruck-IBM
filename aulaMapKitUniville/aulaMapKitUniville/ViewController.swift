bbb3ea2294f67c4c228146b032a8ef9cfd505800
gdguesser <gdguesser@gmail.com>
2020-02-13 21:00:27 -0300

    mapkit app

--- aulaMapKitUniville/aulaMapKitUniville/ViewController.swift
+++ aulaMapKitUniville/aulaMapKitUniville/ViewController.swift
@@ -15,20 +15,20 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var meuMapa: MKMapView!
     
     var locationManager = CLLocationManager()
-    var userManager = CLLocation()
-    
-    
-    
-    
+    var userLocation = CLLocation()
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+            
+        meuMapa.showsUserLocation = true
+        addGesture()
+        setupLocationManager()
         
     }
+    
+    
     
     func setupLocationManager() {
         
@@ -39,6 +39,39 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.requestWhenInUseAuthorization()
         
         locationManager.startUpdatingLocation()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if locations.count > 0 {
+            if let location = locations.last {
+                userLocation = location
+                print("A localização do usuário é \(userLocation.coordinate)")
+            }
+        }
+    }
+    
+    @objc func addAnnotationToMap(gestureRecognizer: UIGestureRecognizer) {
+        
+        let touchPoint = gestureRecognizer.location(in: meuMapa)
+        let newCoordinate: CLLocationCoordinate2D = meuMapa.convert(touchPoint, toCoordinateFrom: meuMapa)
+        let newAnnotation = MKPointAnnotation()
+        
+        newAnnotation.coordinate = newCoordinate
+        newAnnotation.title = "Guesser"
+        newAnnotation.subtitle = String(describing: "Latitude \(newCoordinate.latitude) , Longitude \(newCoordinate.longitude)")
+        
+        meuMapa.addAnnotation(newAnnotation)
+    }
+    
+    func addGesture() {
+        
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(addAnnotationToMap(gestureRecognizer:)))
+        longPress.minimumPressDuration = 0.8
+        
+        meuMapa.addGestureRecognizer(longPress)
+        
         
     }
     
